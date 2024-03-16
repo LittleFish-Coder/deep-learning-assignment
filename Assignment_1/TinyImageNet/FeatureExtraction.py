fe8cf81d5b8685b721c229b6b729b4ebc12a383e
LittleFish-Coder <adrianyu890317@gmail.com>
2024-03-16 20:51:12 +0800

    add ColorHistogram

--- Assignment_1/TinyImageNet/FeatureExtraction.py
+++ Assignment_1/TinyImageNet/FeatureExtraction.py
@@ -38,10 +38,32 @@ def HOG(image):
     return features
 
 
+def ColorHistogram(image, bins=(8, 8, 8)):
+
+    # Convert BGR to HSV
+    HSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # Create a mask for the color range
+    lower = np.array([0, 0, 0])
+    upper = np.array([179, 255, 255])
+    mask = cv2.inRange(HSV, lower, upper)
+
+    # Calculate the histogram
+    hist = cv2.calcHist([HSV], [0, 1, 2], mask, bins, [0, 180, 0, 256, 0, 256])
+
+    # Normalize the histogram
+    hist = cv2.normalize(hist, hist)
+
+    # print(hist.shape) # (8, 8, 8)
+
+    return hist
+
+
 # debug
 # image = cv2.imread("TIN/n01774384/images/n01774384_4.JPEG")
 # image = cv2.resize(image, (256, 256))
 # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # features = HOG(image)
+# features = ColorHistogram(image)
 # print(features.shape)
 # print(features)
